65670b45526817588b11f2fbdb1e91a7423f6033
0xconssize <0xconssize@gmail.com>
2026-06-25 10:31:18 +0200

    Update run-node script to use relative paths for configuration and keys

--- 08-run-node.sh
+++ 08-run-node.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+cd tmp-testnet
+
 cardano-node run \
-  --config leios/config.json \
-  --topology leios/topology.json \
-  --database-path leios/db \
-  --socket-path leios/node.socket \
+  --config config.json \
+  --topology topology.json \
+  --database-path db \
+  --socket-path node.socket \
   --host-addr 0.0.0.0 \
   --port 3010 \
-  --shelley-kes-key leios/keys/kes.skey \
-  --shelley-vrf-key leios/keys/vrf.skey \
-  --shelley-operational-certificate leios/keys/opcert.cert
+  --shelley-kes-key ../leios/keys/kes.skey \
+  --shelley-vrf-key ../leios/keys/vrf.skey \
+  --shelley-operational-certificate ../leios/keys/opcert.cert
