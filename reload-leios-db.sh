6742275e5de2838a929c9fd9e2971065b9dff967
0xconssize <0xconssize@gmail.com>
2026-07-06 11:00:38 +0000

    feat: reload leios db script

---
+++ reload-leios-db.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+systemctl --user stop cardano-node
+mv tmp-testnet/leios.db tmp-testnet/leios.db-$(date +%s)
+mv tmp-testnet/leios.db-shm tmp-testnet/leios.db-shm$(date +%s)
+mv tmp-testnet/leios.db-wal tmp-testnet/leios.db-wal$(date +%s)
+RELAY_FQDN=leios1-rel-a-1.play.dev.cardano.org
+wget -c --tries=10 --retry-connrefused  --waitretry=10 --read-timeout=120  https://$RELAY_FQDN/leios.leiosdb.tar.zst
+wget https://$RELAY_FQDN/leios.leiosdb.tar.zst.sha256
+sha256sum -c leios.leiosdb.tar.zst.sha256
+tar -xf leios.leiosdb.tar.zst -C tmp-testnet
+systemctl --user stop cardano-node
