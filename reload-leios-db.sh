b48ea44456e76b06f64dd009a12e74d48f31bc31
0xconssize <0xconssize@gmail.com>
2026-07-06 11:38:06 +0000

    feat: reload db

--- reload-leios-db.sh
+++ reload-leios-db.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 systemctl --user stop cardano-node
+rm leios.leiosdb.*
 mv tmp-testnet/leios.db tmp-testnet/leios.db-$(date +%s)
 mv tmp-testnet/leios.db-shm tmp-testnet/leios.db-shm$(date +%s)
 mv tmp-testnet/leios.db-wal tmp-testnet/leios.db-wal$(date +%s)
@@ -9,4 +10,4 @@ wget -c --tries=10 --retry-connrefused  --waitretry=10 --read-timeout=120  https
 wget https://$RELAY_FQDN/leios.leiosdb.tar.zst.sha256
 sha256sum -c leios.leiosdb.tar.zst.sha256
 tar -xf leios.leiosdb.tar.zst -C tmp-testnet
-systemctl --user stop cardano-node
+systemctl --user start cardano-node
